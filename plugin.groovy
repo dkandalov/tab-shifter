1678d2d3b9106d1c1094678ab04ed5415c861427
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-05-25 17:26:29 +0100

    loading as a live plugin

---
+++ plugin.groovy
@@ -0,0 +1,27 @@
+import com.intellij.openapi.actionSystem.AnActionEvent
+import socrates.tabshifter.Actions
+
+import static liveplugin.PluginUtil.registerAction
+import static liveplugin.PluginUtil.show
+
+// add-to-classpath $HOME/Library/Application Support/IntelliJIdea13/live-plugins/tab-shift/out/production/tab-shifter
+
+if (isIdeStartup) return
+
+def shiftLeft = new Actions.ShiftLeft()
+registerAction("TabShiftActions.ShiftLeft",
+        "alt shift OPEN_BRACKET",
+        "EditorTabsGroup",
+        "Shift Left") { AnActionEvent event ->
+    shiftLeft.actionPerformed(event)
+}
+def shiftRight = new Actions.ShiftLeft()
+registerAction("TabShiftActions.ShiftRight",
+        "alt shift CLOSE_BRACKET",
+        "EditorTabsGroup",
+        "Shift Right") { AnActionEvent event ->
+    shiftRight.actionPerformed(event)
+}
+
+
+show("Reloaded Tab Shifter plugin")
