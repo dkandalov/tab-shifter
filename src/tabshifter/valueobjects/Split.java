54e0addd166d754400fc096ced1cb96234edfa40
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-08-19 08:25:23 +0100

    formatting

--- src/tabshifter/valueobjects/Split.java
+++ src/tabshifter/valueobjects/Split.java
@@ -18,9 +18,17 @@ public class Split extends LayoutElement {
     }
 
     @Override public Size size() {
-        return orientation == Orientation.vertical ?
-            new Size(first.size().width + second.size().width, max(first.size().height, second.size().height)) :
-            new Size(max(first.size().width, second.size().width), first.size().height + second.size().height);
+        if (orientation == Orientation.vertical) {
+            return new Size(
+                    first.size().width + second.size().width,
+                    max(first.size().height, second.size().height)
+            );
+        } else {
+            return new Size(
+                    max(first.size().width, second.size().width),
+                    first.size().height + second.size().height
+            );
+        }
     }
 
     @Override public String toString() {
