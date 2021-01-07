2cc9a875b6bfb9ae0f2e7e0a975acaaf517296a7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-01-07 23:58:20 +0000

    refactoring

--- src/tabshifter/EditorWindow_AccessToPanel_Hack.kt
+++ src/tabshifter/EditorWindow_AccessToPanel_Hack.kt
@@ -2,26 +2,21 @@ package tabshifter
 
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.impl.EditorWindow
-import java.lang.reflect.Field
 import javax.swing.JPanel
 
 object EditorWindow_AccessToPanel_Hack {
     private val logger = Logger.getInstance(TabShifter::class.java.name)
 
-    fun panelOf(editorWindow: EditorWindow?): JPanel? {
-        val panelField: Field
+    fun panelOf(editorWindow: EditorWindow?): JPanel? =
         try {
-            panelField = EditorWindow::class.java.getDeclaredField("myPanel")
+            val panelField = EditorWindow::class.java.getDeclaredField("myPanel")
             panelField.isAccessible = true
+            panelField[editorWindow] as JPanel
         } catch (e: NoSuchFieldException) {
             logger.warn("Couldn't find myPanel field")
-            return null
-        }
-        return try {
-            panelField[editorWindow] as JPanel
+            null
         } catch (e: IllegalAccessException) {
             logger.warn("Couldn't access myPanel field", e)
             null
         }
-    }
 }
